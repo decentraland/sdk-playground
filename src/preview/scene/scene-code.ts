397c8dea9b3d47c896308118296abbfa1520488c
Leandro Mendoza <leandro@decentraland.org>
2023-04-05 22:52:36 -0300

    fix: new build playground assets

--- src/preview/scene/scene-code.ts
+++ src/preview/scene/scene-code.ts
@@ -1,21 +1,14 @@
 import { PackagesData } from '../../utils/bundle/types'
 import { compileScene } from '../swc-compile'
 
-export async function getPreviewCode(packageData: PackagesData, code: string) {
-  const gameJsTemplate = packageData?.scene.js
-  const codeToAddFirst = `
-const EngineApi = require('~system/EngineApi');
-const communicationsController = require('~system/CommunicationsController');
-const EthereumController = require('~system/EthereumController');
-const process = {env: {}};
-        ${gameJsTemplate}
-exports.onUpdate = globalThis.onUpdate
-exports.onStart = globalThis.onStart
-      `
-  const codeToCompile = packageData?.scene.types + ';' + code
+export async function getPreviewCode(packageData: PackagesData, editorCode: string) {
+  const codeToCompile = packageData?.scene.types + ';' + editorCode
   const compiledCode = await compileScene(codeToCompile)
   return `
-${codeToAddFirst};
+${packageData?.scene.js};
+for (const key in exports) {
+  this.globalThis[key] = exports[key]
+}
 eval(${JSON.stringify(compiledCode)})
-`
+  `
 }
