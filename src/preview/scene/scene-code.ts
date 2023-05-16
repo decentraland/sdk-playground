55e5c405ecd906b2a29516c7ec5b4063ce9dc0ba
Lean Mendoza <leandro@decentraland.org>
2023-05-16 13:28:42 -0300

    fix: add `use strict`

--- src/preview/scene/scene-code.ts
+++ src/preview/scene/scene-code.ts
@@ -5,6 +5,7 @@ export async function getPreviewCode(packageData: PackagesData, editorCode: stri
   const codeToCompile = packageData?.scene.types + ';' + editorCode
   const compiledCode = await compileScene(codeToCompile)
   return `
+  'use strict';
 ${packageData?.scene.js};
 for (const key in exports) {
   this.globalThis[key] = exports[key]
